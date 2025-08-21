e00a1ebb193309899ce3998e89e3c471749f3ef2
Scott Condit <scott@socode.com>
2025-08-21 19:54:04 +0100

    done

--- src/object.rs
+++ src/object.rs
@@ -1,3 +1,26 @@
+#[macro_export]
+macro_rules! ObjectBase {
+    (#[derive($($derive:meta),*)] $pub:vis struct $name:ident { $($fpub:vis $field:ident : $type:ty,)* }) => {
+        #[derive($($derive),*)]
+        #[repr(C)]
+        $pub struct $name {
+            pub next: *mut Object,
+            pub tag: u8,
+            pub marked: u8,
+            $($fpub $field : $type,)*
+        }
+        impl $name {
+            $pub fn new(next: *mut Object, tag: u8, marked: u8, $($field:$type,)*) -> Self{
+                Self{
+                    next,
+                    tag,
+                    marked,
+                    $($field,)*
+                }
+            }
+        }
+    }
+}
 #[derive(Copy, Clone)]
 pub struct Object {
     pub next: *mut Object,
--- src/table.rs
+++ src/table.rs
@@ -1,12 +1,11 @@
 use crate::node::*;
 use crate::object::*;
 use crate::stkidrel::*;
-#[derive(Copy, Clone)]
-#[repr(C)]
+use crate::ObjectBase;
+
+ObjectBase! {
+#[derive(Debug, Copy, Clone)]
 pub struct Table {
-    pub next: *mut Object,
-    pub tag: u8,
-    pub marked: u8,
     pub flags: u8,
     pub lsizenode: u8,
     pub alimit: u32,
@@ -15,4 +14,5 @@ pub struct Table {
     pub lastfree: *mut Node,
     pub metatable: *mut Table,
     pub gc_list: *mut Object,
+}
 }
