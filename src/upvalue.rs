cd6c68ab23c8323a4719bea4fbd283cebc47116d
Scott Condit <scott@socode.com>
2025-08-28 13:14:08 +0100

    done

--- src/upvalue.rs
+++ src/upvalue.rs
@@ -5,30 +5,28 @@ use crate::tvalue::*;
 #[derive(Copy, Clone)]
 #[repr(C)]
 pub struct UpValue {
-    pub next: *mut Object,
-    pub tag: u8,
-    pub marked: u8,
+    pub object: Object,
     pub v: UpValueA,
     pub u: UpValueB,
 }
 impl TObject for UpValue {
     fn get_marked(&self) -> u8 {
-        self.marked
+        self.object.get_marked()
     }
     fn set_marked(&mut self, marked_: u8) {
-        self.marked = marked_;
+        self.object.set_marked(marked_);
     }
     fn set_tag(&mut self, tag: u8) {
-        self.tag = tag;
+        self.object.set_tag(tag);
     }
     fn set_collectable(&mut self) {
-        self.set_tag(set_collectable(self.get_tag()));
+        self.object.set_collectable();
     }
     fn is_collectable(&self) -> bool {
-        return is_collectable(self.get_tag());
+        return self.object.is_collectable();
     }
     fn get_tag(&self) -> u8 {
-        return self.tag;
+        return self.object.get_tag();
     }
     fn get_tag_type(&self) -> u8 {
         return get_tag_type(self.get_tag());
